0069b99da8b3a4fbab28c600615ba113dcd53412
Donald Steinert <steinert.donald@gmail.com>
2015-07-27 13:25:19 -0400

    complete invoke

--- src/underbar.js
+++ src/underbar.js
@@ -420,6 +420,19 @@
   // Calls the method named by functionOrKey on each value in the list.
   // Note: You will need to learn a bit about .apply to complete this.
   _.invoke = function(collection, functionOrKey, args) {
+    
+    args = Array.prototype.slice.call(arguments, 2);
+    
+    if (typeof functionOrKey === 'function') { 
+      return _.map(collection, function(item) {
+        return functionOrKey.apply(item, args);
+      });
+    } else {
+      return _.map(collection, function(item) {
+        return item[functionOrKey].apply(item, args);
+      });
+    }
+    
   };
 
   // Sort the object's values by a criterion produced by an iterator.
